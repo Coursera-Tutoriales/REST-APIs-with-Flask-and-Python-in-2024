a17f0a6ea6a2b8cf02ec01222cecd3056fae68c0
danielsierra34 <danielsierra34@gmail.com>
2025-03-04 16:36:21 -0500

    se crea el endppoint para crear una tienda y se importa request de flask para obtener el json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 # se crea una instancia de Flask en app
 app = Flask(__name__)
@@ -15,6 +15,14 @@ stores = [
     }
 ]
 
-@app.get("/stores")
+@app.get("/store")
 def get_stores():
     return {"stores":stores}
+
+@app.post("/store")
+def create_store():
+    # se almacena el json en un diccionario
+    request_data = request.get_json()
+    new_store = {"name" : request_data["name"], "items" : []}
+    stores.append(new_store)
+    return new_store, 201
