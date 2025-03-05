d23beb0679dd8d67b931ff9a8fe9c3577458da7a
danielsierra34 <danielsierra34@gmail.com>
2025-03-04 19:09:11 -0500

    se agrega el endpoint para listar los items de un store

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ stores = [
 def get_stores():
     return {"stores":stores}
 
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store, 201
+    return {"message" : "Store not found"}, 404
+
 @app.post("/store")
 def create_store():
     # se almacena el json en un diccionario
@@ -26,3 +33,21 @@ def create_store():
     new_store = {"name" : request_data["name"], "items" : []}
     stores.append(new_store)
     return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item_in_store(name):
+    # se almacena el json en un diccionario
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message" : "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:            
+            return {"items":store["items"]}, 201
+    return {"message" : "Store not found"}, 404
