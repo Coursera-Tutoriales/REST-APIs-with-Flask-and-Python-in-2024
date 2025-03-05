15bee96085cd6fa41043f845cfb6b0a7e3d9f092
danielsierra34 <danielsierra34@gmail.com>
2025-03-05 16:20:54 -0500

    se extrae el crud de app.py y se coloca en los resources implementando method views

---
+++ resources/store.py
@@ -0,0 +1,51 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import stores
+
+blp = Blueprint("Stores", __name__, description="Operations on stores")
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    def get(self, store_id):
+        try:
+            return stores[store_id], 200
+        except KeyError:
+            abort(404, message="Store not found")
+
+    def delete(self, store_id):
+        try:
+            del stores[store_id]
+            return {"message":"store deleted"}
+        except KeyError:
+            abort(404, message="store not found")
+
+    def put(self, store_id):
+        store_data = request.get_json()
+        if ("name" not in store_data):
+            abort(400, message="Bad request. Ensure 'name' is included.")
+        try:
+            store = stores[store_id]
+            store |= store_data
+            return {"message":"store updated"}
+        except KeyError:
+            abort(404, message="store not found")
+
+@blp.route("/store")
+class StoreList(MethodView):
+    def get(self):
+        return {"stores":list(stores.values())}
+
+    def post(self):
+        store_data = request.get_json()
+        if "name" not in store_data:
+            abort (400, message="Bad Request please ensure name is included")
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message="Store already Exists")
+
+        store_id = uuid.uuid4().hex
+        store = {**store_data, "id":store_id}
+        stores[store_id] = store
+        return store, 201
